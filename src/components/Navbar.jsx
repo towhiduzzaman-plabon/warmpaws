e3aee02b4d1e0d68015c84a6401ac202fba39c78
Towhiduzzaman Plabon <plabonofficial24@gmail.com>
2025-10-27 22:09:39 +0600

    feat: add Firebase authentication (Signup, Login, Google Sign-in)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import { AuthContext } from '../contexts/AuthProvider'
-
+//lovelovelove
 const Navbar = () => {
   const { user, logout } = useContext(AuthContext)
 
