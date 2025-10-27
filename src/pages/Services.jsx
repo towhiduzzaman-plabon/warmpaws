35fcddb8a54c381eb467d7b4fd825a47d2c2f1b4
Towhiduzzaman Plabon <plabonofficial24@gmail.com>
2025-10-27 22:21:36 +0600

    feat: add SPA fallback (200.html) and Surge deployment configuration

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -19,4 +19,3 @@ const Services = () => {
 }
 
 export default Services
-//jsfhshfishfisdjfksdhfsdufshdfishdfshd
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -75,3 +75,4 @@ const Signup = () => {
 }
 
 export default Signup
+////jfjfdgjdfhgihfgihigguhghfg
